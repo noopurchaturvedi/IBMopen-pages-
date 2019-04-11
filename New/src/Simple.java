8fa45a88be94cc0383d6b66789b98a102b0e89f5
noopur.chaturvedi <noopur.chaturvedi@BDC6-DX-87RMX52.dir.svc.accenture.com>
2019-04-11 04:09:41 -0500

    new commit by noopur

--- New/src/Simple.java
+++ New/src/Simple.java
@@ -2,6 +2,10 @@
 public class Simple {
 
 	public static void main(String args[]){
+    int a = 10;
+    int b = 30;
+    int c = a + b;
+    System.out.println(c);
 	System.out.println("Hello Tina ji");
 	System.out.println("How are you");
 }
