efb17620e427be05ac91913130f561d0d4cf59b3
noopur.chaturvedi <noopur.chaturvedi@BDC6-DX-87RMX52.dir.svc.accenture.com>
2019-04-11 04:13:28 -0500

    final cpommit by tinA

--- New/src/Simple.java
+++ New/src/Simple.java
@@ -4,7 +4,9 @@ public class Simple {
 	public static void main(String args[]){
     int a = 10;
     int b = 30;
+    boolean d = false;
     int c = a + b;
+    System.out.println(d);
     System.out.println(c);
 	System.out.println("Hello Tina ji");
 	System.out.println("How are you");
